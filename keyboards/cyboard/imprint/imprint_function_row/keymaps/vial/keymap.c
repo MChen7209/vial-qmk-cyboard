93f46e959cda1db4e06b888d4becac35bc8a8741
Michael Chen <michael.chen@genesys.com>
2025-11-29 20:28:05 -0600

    Add some LT's

--- keyboards/cyboard/imprint/imprint_function_row/keymaps/vial/keymap.c
+++ keyboards/cyboard/imprint/imprint_function_row/keymaps/vial/keymap.c
@@ -19,13 +19,13 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_fun(
-        KC_F11,  KC_F1, KC_F2,   KC_F3,   KC_F4,   KC_F5,                               KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F12,
-        KC_GRV,  KC_1,  KC_2,    KC_3,    KC_4,    KC_5,                                KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_TAB,  KC_Q,  KC_W,    KC_F,    KC_P,    KC_B,                                KC_J,    KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
- LSFT_T(KC_ESC), LCTL_T(KC_A), LALT_T(KC_R), LGUI_T(KC_S), LSFT_T(KC_T), KC_G,          KC_M, RSFT_T(KC_N), RGUI_T(KC_E), RALT_T(KC_I), RCTL_T(KC_O), KC_QUOT,
-        KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_D,    KC_V,                                KC_K,    KC_H,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LBRC, KC_RBRC, KC_LCTL, KC_LGUI, KC_ESC,            MS_BTN1, MS_BTN2, KC_SPC, KC_EQL,  KC_MINS,
-                                          KC_BSPC, KC_LALT, MO(3),             MS_BTN3, KC_ENT,  TD(0)
+        KC_F11,  KC_F1, KC_F2,       KC_F3,   KC_F4,   KC_F5,                               KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
+        KC_GRV,  KC_1,  KC_2,        KC_3,    KC_4,    KC_5,                                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_TAB,  KC_Q,  LT(1, KC_W), KC_F,    KC_P,    KC_B,                                KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
+ LSFT_T(KC_ESC), LCTL_T(KC_A), LALT_T(KC_R), LGUI_T(KC_S), LSFT_T(KC_T), LT(4, KC_G),       KC_M,    RSFT_T(KC_N), RGUI_T(KC_E), RALT_T(KC_I), RCTL_T(KC_O), KC_QUOT,
+        KC_LSFT, KC_Z,  KC_X,        KC_C,    KC_D,    KC_V,                                KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                        KC_LBRC,     KC_RBRC, KC_LCTL, KC_LGUI, KC_ESC,            MS_BTN1, MS_BTN2, KC_RGUI, KC_EQL,  KC_MINS,
+                                          KC_BSPC, KC_LALT, MO(3),                 MS_BTN3, KC_ENT,  LT(1, KC_SPC)
     ),
 
     [1] = LAYOUT_fun(
